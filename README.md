5be37a235cc1b17db0d5c66567c775e942099204
daiki <daiking_tensai@yahoo.co.jp>
2020-04-08 21:37:48 +0900

    add USB Device(light check OK)

--- README.md
+++ README.md
@@ -24,7 +24,12 @@ mbed host application for PS2 controller
 C:\WorkSpace\mbed-repo\mbedos2-cli-example-basic>mbed compile -m LPC1768 -t GCC_ARM
 C:\WorkSpace\mbed-repo\mbedos2-cli-example-basic>copy BUILD\LPC1768\GCC_ARM\mbedos2-cli-ps2controller-host D:\
 ```
-
+or
+```txt
+C:\WorkSpace\mbed-repo\mbedos2-cli-example-basic>mbed compile -c -m LPC1768 -t GCC_ARM
+C:\WorkSpace\mbed-repo\mbedos2-cli-example-basic>copy BUILD\LPC1768\GCC_ARM\mbedos2-cli-ps2controller-host D:\
+```
+(clean build)
 
 ### Related Links
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include "mbed.h"
-
-DigitalOut led(LED1);
-
-int main()
-{
-    printf("hello, Mbed.\n");
+#include "USBMouseKeyboard.h"
+ 
+//LED1: NUM_LOCK
+//LED2: CAPS_LOCK
+//LED3: SCROLL_LOCK
+BusOut leds(LED1, LED2, LED3);
+ 
+//USBMouseKeyboard
+USBMouseKeyboard key_mouse;
+ 
+int main(void) {
     while (1) {
-        led = !led;
-        wait(0.5);
+        // key_mouse.mediaControl(KEY_VOLUME_DOWN);
+        key_mouse.printf("Hello World from Mbed\r\n");
+        // key_mouse.keyCode('s', KEY_CTRL);
+        key_mouse.move(20, 0);
+        wait(1);
+        key_mouse.move(0, 20);
+        wait(1);
+        key_mouse.move(0, -20);
+        wait(1);
+        key_mouse.move(-20, 0);
+        wait(1);
+        // key_mouse.keyCode(KEY_SCROLL_LOCK);
+        wait(1);
+        // leds = key_mouse.lockStatus();
     }
 }
